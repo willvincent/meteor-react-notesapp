fd8c78d16a62745117379905ae0eba251667ef8e
Will Vincent <will@willvincent.com>
2017-04-15 03:53:44 -0500

    Mobile styles & off-canvas nav

--- client/main.js
+++ client/main.js
@@ -16,12 +16,20 @@ Tracker.autorun(() => {
 
 Tracker.autorun(() => {
   let selectedNoteId = Session.get('selectedNoteId')
+  Session.set('navOpen', false)
+  
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`)
   }
 })
 
+Tracker.autorun(() => {
+  let navOpen = Session.get('navOpen')
+  document.body.classList.toggle('nav-open', navOpen)
+})
+
 Meteor.startup(() => {
   Session.set('selectedNoteId', undefined)
+  Session.set('navOpen', false)
   ReactDOM.render(routes, document.getElementById('app'))
 })
--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Accounts } from 'meteor/accounts-base'
 import PropTypes from 'prop-types'
 import { createContainer } from 'meteor/react-meteor-data'
+import { Session } from 'meteor/session'
 
 export const PrivateHeader = (props) => {
+  let navImg = props.navOpen ? '/images/x.svg' : '/images/bars.svg'
+
   return (
     <div className="header">
       <div className="header__content">
+        <img className="header__nav-toggle" src={ navImg } onClick={ props.toggleNav } />
         <h1 className="header__title">{ props.title }</h1>
         <button 
           className="button button--link-text"
@@ -18,11 +22,15 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
   title: PropTypes.string.isRequired,
-  handleLogout: PropTypes.func.isRequired
+  handleLogout: PropTypes.func.isRequired,
+  navOpen: PropTypes.bool.isRequired,
+  toggleNav: PropTypes.func.isRequired
 }
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
+    handleLogout: () => Accounts.logout(),
+    toggleNav: () => Session.set('navOpen', !Session.get('navOpen')),
+    navOpen: Session.get('navOpen')
   }
 }, PrivateHeader)
