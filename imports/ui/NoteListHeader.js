823c5d6946a7fd1ef2e6741cbecf932c6e687b96
Will Vincent <will@willvincent.com>
2017-04-15 01:18:43 -0500

    Update sort order of notes, auto-select newly created notes.

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -2,22 +2,29 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Notes } from '../api/notes'
+import { Session } from 'meteor/session'
 import PropTypes from 'prop-types'
 
 export const NoteListHeader = (props) => {
   return (
     <div>
-      <button onClick={ () => props.meteorCall('notes.insert') }>+ New Note</button>
+      <button onClick={ () => props.meteorCall('notes.insert', (err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res)
+          }
+        }) }>+ New Note</button>
     </div>
   )
 }
 
 NoteListHeader.propTypes = {
-  meteorCall: PropTypes.func.isRequired
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   }
 }, NoteListHeader)
--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -4,15 +4,38 @@ import expect from 'expect'
 import { mount } from 'enzyme'
 
 import { NoteListHeader } from './NoteListHeader'
+import { notes } from '../fixtures/fixtures'
 
 if (Meteor.isClient) {
   describe('NoteListHeader', function () {
+    let meteorCall;
+    let Session;
+
+    beforeEach(() => {
+      meteorCall = expect.createSpy()
+      Session = {
+        set: expect.createSpy()
+      }
+    })
     
     it('Should call meteorCall on click', function () {
-      let spy = expect.createSpy()
-      let wrapper = mount( <NoteListHeader meteorCall={spy}/> )
+      let wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> )
+      
       wrapper.find('button').simulate('click')
-      expect(spy).toHaveBeenCalledWith('notes.insert')
+      meteorCall.calls[0].arguments[1](undefined, notes[0]._id)
+      
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert')
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id)
+    })
+
+    it('Should not update selectedNoteId if new note is not created', function () {
+      let wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> )
+      
+      wrapper.find('button').simulate('click')
+      meteorCall.calls[0].arguments[1]({}, undefined)
+
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert')
+      expect(Session.set).toNotHaveBeenCalled()
     })
 
   })
